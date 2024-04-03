379c86e58ad9b2ebcc7fbc26014a1e96e2f00b1e
Anna Zorina <zorinannaa@gmail.com>
2024-04-03 19:13:29 +0300

    commit 1, first try

--- scripts/index.js
+++ scripts/index.js
@@ -7,3 +7,28 @@
 // @todo: Функция удаления карточки
 
 // @todo: Вывести карточки на страницу
+
+const cardTemplate = document.querySelector('#card-template').content;
+const cardList = document.querySelector('.places__list');
+
+function deleteCard (card) {
+  const cardItem = document.querySelector('.card');
+
+  cardItem.remove();
+};
+
+function showCard (cards, deleteFunc) {
+  cards.forEach(function (item) {
+    const cardTemplateClone = cardTemplate.cloneNode(true);
+    cardTemplateClone.querySelector('.card__image').src = item.link;
+    cardTemplateClone.querySelector('.card__title').textContent = item.name;
+    const deleteButton = cardTemplateClone.querySelector('.card__delete-button');
+  
+    deleteButton.addEventListener('click', deleteFunc);
+  
+    cardList.append(cardTemplateClone);
+    return cardList
+  });
+};
+
+showCard(initialCards, deleteCard);
